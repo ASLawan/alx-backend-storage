1d34bbca11e1f4552a359394740de192eef1b9f4
ASLawan <nawalnitsua@gmail.com>
2024-06-13 12:25:13 +0100

    Use SUM in place of COUNT

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- script to group number of fans by country of origin bands
 
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
