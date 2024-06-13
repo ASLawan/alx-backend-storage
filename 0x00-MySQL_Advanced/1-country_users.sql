556a7a54b7826f834190516129c5a63118e4b043
ASLawan <nawalnitsua@gmail.com>
2024-06-13 11:54:35 +0100

    Write SQL script that creates users table with given requirements

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- script to create table 'users' with given requirements
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+
+);
