9db5d58c92bc9feaaf4b6cbe6a1365ff1cb8ba71
ASLawan <nawalnitsua@gmail.com>
2024-06-18 16:40:40 -0700

    Count nginx logs

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+    Module implementing code that retrieves stats about nginx
+    logs
+
+"""
+from pymongo import MongoClient
+
+
+try:
+    client = MongoClient('mongodb://127.0.0.1:27017/')
+    db = client['logs']
+    collection = db['nginx']
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+    print("Methods:")
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        count = collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {count}")
+    get_collection = collection.find({"method": "GET", "path": "/status"})
+    count = 0
+    for item in get_collection:
+        count += 1
+    print(f"{count} status check")
+except Exception as e:
+    print(f"Error as {e}")
+finally:
+    client.close()
