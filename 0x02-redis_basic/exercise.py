b6af1d21d96399cb524885099f826d6259851938
ASLawan <nawalnitsua@gmail.com>
2024-06-19 15:12:58 +0100

    Update- add more methods to Cache class- return number of times a method gets called

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,8 +4,18 @@
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
+from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    """Returns number of times a method has been called"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     """Instantiates instances of redis client"""
@@ -14,6 +24,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Takes a data argument and returns a string"""
         key = str(uuid.uuid4())
