db8a4d42d1fa5468628b3da4270f79a8abb4742e
ASLawan <nawalnitsua@gmail.com>
2024-06-19 14:39:01 +0100

    Update Cache class - add more methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,3 +19,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> \
+            Union[str, bytes, int, float, None]:
+        """Converts data back to desired format"""
+        result = self._redis.get(key)
+        if result is None:
+            return None
+        if fn:
+            return fn(result)
+        return result
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """Returns string"""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """Returns an integer"""
+        return self.get(key, int)
