9c1e928065b0c5917f75cf5e9f2c7b281cd2dade
ASLawan <nawalnitsua@gmail.com>
2024-06-19 17:17:36 -0700

    Fix pycodestyle compliance errors

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,24 +7,30 @@
 from pymongo import MongoClient
 
 
-try:
-    client = MongoClient('mongodb://127.0.0.1:27017/')
-    db = client['logs']
-    collection = db['nginx']
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
-    print("Methods:")
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    space = " " * 4
-    for method in methods:
-        count = collection.count_documents({'method': method})
-        print(f"{space}method {method}: {count}")
-    get_collection = collection.find({"method": "GET", "path": "/status"})
-    count = 0
-    for item in get_collection:
-        count += 1
-    print(f"{count} status check")
-except Exception as e:
-    print(f"Error as {e}")
-finally:
-    client.close()
+def print_logs():
+    """Prints log stats"""
+    try:
+        client = MongoClient('mongodb://127.0.0.1:27017/')
+        db = client['logs']
+        collection = db['nginx']
+        total_logs = collection.count_documents({})
+        print(f"{total_logs} logs")
+        print("Methods:")
+        methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+        space = " " * 4
+        for method in methods:
+            count = collection.count_documents({'method': method})
+            print(f"{space}method {method}: {count}")
+        get_collection = collection.find({"method": "GET", "path": "/status"})
+        count = 0
+        for item in get_collection:
+            count += 1
+        print(f"{count} status check")
+    except Exception as e:
+        print(f"Error as {e}")
+    finally:
+        client.close()
+
+
+if __name__ == "__main__":
+    print_logs()
